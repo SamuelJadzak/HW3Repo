40af4a93ebcc9658a1e8ec47d4542b424f4fae3f
SamuelJadzak <43551888+SamuelJadzak@users.noreply.github.com>
2021-04-25 23:13:52 -0700

    Error handling

--- Samuel_Jadzak_hw1.py
+++ Samuel_Jadzak_hw1.py
@@ -1,4 +1,8 @@
-year = int(input("Please enter a year: "))
+inputYear = input("Please enter a year: ")
+try:
+    year = int(inputYear)
+except ValueError:
+    print('\nThat is not a valid integer')
 if(year%100==0 and year%400!=0):
     print(str(year) +" is not a leap year")
 elif(year%4==0): 
